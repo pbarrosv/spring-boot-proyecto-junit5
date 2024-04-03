b18cd7dd9b5f67e5bdbb6e2d7dfed378b816c7de
pbarrosv <pedroluisbarrosvalle@hotmail.com>
2024-04-02 19:43:38 -0500

    Proyeto spring-boot-proyecto-junit5

---
+++ src/main/java/com/app/junit/project/SpringBootProyectoJunit5Application.java
@@ -0,0 +1,13 @@
+package com.app.junit.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProyectoJunit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProyectoJunit5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/junit/project/SpringBootProyectoJunit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.junit.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProyectoJunit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
